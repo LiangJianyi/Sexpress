373f6174b8f3a62653bfe2d76f797da6723265f5
梁建溢 <a.124p@live.com>
2017-10-28 16:58:20 +0800

    Add out of range exception message in set-mcar-by-ref! proc

--- liangjianyi-racket/LinkedList.rkt
+++ liangjianyi-racket/LinkedList.rkt
@@ -89,16 +89,18 @@
       (prepend-linkedlist (mcons [mcar lik] null) (linkedlist-reverse [mcdr lik]))))
 
 (define (set-mcar-by-ref! lik ref node)
-  (letrec ((n (linkedlist-length lik))
-           (temp null)
-           (f (lambda (i k aux)
-                (if (= i ref)
-                    (begin
-                      (set! temp [append-linkedlist aux (mcons node [mcdr k])])
-                      (set-mcar! lik (mcar temp))
-                      (set-mcdr! lik (mcdr temp)))
-                    (f (+ i 1) (mcdr k) (append-linkedlist aux (mcons (mcar k) null)))))))
-    (f 0 lik null)))
+  (if (>= ref [linkedlist-length lik])
+      (error "ref: out of range exception.")
+      (letrec ((n (linkedlist-length lik))
+               (temp null)
+               (f (lambda (i k aux)
+                    (if (= i ref)
+                        (begin
+                          (set! temp [append-linkedlist aux (mcons node [mcdr k])])
+                          (set-mcar! lik (mcar temp))
+                          (set-mcdr! lik (mcdr temp)))
+                        (f (+ i 1) (mcdr k) (append-linkedlist aux (mcons (mcar k) null)))))))
+        (f 0 lik null))))
 
 (define (list->linkedlist list)
   (letrec ((f (lambda (list lik)
