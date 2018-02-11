b40f70724cfcb75eb23b20f60347b12db35c3793
梁建溢 <a.124p@live.com>
2018-02-11 06:49:05 -0500

    Changesets-40

--- Scheme/test-multivariantree.rkt
+++ Scheme/test-multivariantree.rkt
@@ -1,8 +1,49 @@
 #lang racket
-(require liangjianyi-racket/MultivariantTree)
+;(require liangjianyi-racket/MultivariantTree)
+(require liangjianyi-racket/LinkedList)
 
-(define top-tree (list null null null))
-(define left-tree (list ('(1)) ('(2))))
-(define medium-tree (list 3 4 5 (list 7 8 '(-1)) 6))
-(define right-tree (list (list '(10 11) 9)))
-(append-multitree '(1 2 3) (cons -1 null))
+;(define top-tree (list null))
+;(define left-tree (list ('(1)) ('(2))))
+;(define medium-tree (list 3 4 5 (list 7 8 '(-1)) 6))
+;(define right-tree (list (list '(10 11) 9)))
+
+(define tree (linkedlist
+              (linkedlist (linkedlist "def" (linkedlist "f") "a")
+                          (linkedlist "f"))))
+
+(define (append-multitree! t n coords
+                           [v (vector (vector 0 0))]
+                           [x 0]
+                           [y 0]
+                           [current-index 0]
+                           [next-index (+ current-index 1)])
+  (unless [>= current-index (- (vector-length coords) 2)]
+    (cond [[and (null? t) (equal? v (vector (vector 0 0)))]
+           (set! t n)]
+          [[mpair? t]
+           (cond [[equal? (vector-ref v current-index) (vector-ref coords current-index)]
+                  ; 一定要先判断左值再判断右值
+                  (cond [[> (vector-ref (vector-ref coords next-index) 0) (vector-ref (vector-ref v current-index) 0)]
+                         (append-multitree! [mcar t] n coords
+                                            (vector-append v (vector (vector [+ x 1] y)))
+                                            [+ x 1]
+                                            y
+                                            [+ current-index 1]
+                                            [+ next-index 1])]
+                        [[> (vector-ref (vector-ref coords next-index) 1) (vector-ref (vector-ref v current-index) 1)]
+                         (append-multitree! [mcdr t] n coords
+                                            (vector-append v (vector (vector x (+ y 1))))
+                                            x
+                                            [+ y 1]
+                                            [+ current-index 1]
+                                            [+ next-index 1])]
+                        [(raise-argument-error 'coords "下一节点的坐标值必须大于上一个节点的坐标值" 2 t n coords v x y current-index next-index)])]
+                 [else (raise-argument-error 'coords "坐标值非法" 2 t n coords v x y current-index next-index)])])))
+
+(define coord (vector (vector 0 0)
+                      (vector 1 0)
+                      (vector 2 0)
+                      (vector 2 1)
+                      (vector 2 2)))
+(append-multitree! tree (mcons -1 null) coord)
+tree
