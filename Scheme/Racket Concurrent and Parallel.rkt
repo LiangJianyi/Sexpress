89bd44ad21a18db809bb994da76ad9193efed537
梁建溢 <a.124p@live.com>
2019-01-26 03:11:27 -0500

    Changesets-53

--- Scheme/Racket Concurrent and Parallel.rkt
+++ Scheme/Racket Concurrent and Parallel.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (define global -1)
+(define cust (make-custodian (current-custodian)))
 
 (define (f1)
   (letrec [[fuck (* global global)]
@@ -12,17 +13,24 @@
 (define (f2) (set! global (+ global 100)))
 
 (define t1 (thread f1))
-;(call-in-nested-thread f1 (current-custodian))
-;(call-in-nested-thread f2 (current-custodian))
+;(call-in-nested-thread f1 (cust))
+;(call-in-nested-thread f2 (cust))
 
 (define (f3)
   (define (f3-1)
     (+ (+ 1 2)
        (call-in-nested-thread (lambda () (+ (+ 1 3)
                                             (call-in-nested-thread (lambda () (+ 1 4))
-                                                                   (current-custodian))))
-                              (current-custodian))))
-  (call-in-nested-thread f3-1 (current-custodian)))
+                                                                   (cust))))
+                              (cust))))
+  (call-in-nested-thread f3-1 (cust)))
 
-
+(custodian-shut-down? cust)
+(custodian-shutdown-all cust)
+(custodian-shut-down? cust)
 (custodian-shut-down? (current-custodian))
+
+'------------------------
+(define custbox (make-custodian-box (current-custodian) 123))
+(custodian-box? custbox)
+(custodian-box-value custbox)
