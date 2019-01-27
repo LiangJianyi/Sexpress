a4ea2b593d38e2830c108561537f15232932088a
梁建溢 <a.124p@live.com>
2019-01-27 11:28:16 -0500

    Changesets-55

--- Scheme/Racket Concurrent and Parallel.rkt
+++ Scheme/Racket Concurrent and Parallel.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require compatibility/mlist)
 (define global -1)
 (define cust (make-custodian (current-custodian)))
 
@@ -13,8 +14,9 @@
 (define (f2) (set! global (+ global 100)))
 
 (define t1 (thread f1))
-(call-in-nested-thread f1 cust)
-(call-in-nested-thread f2 cust)
+(define t2 (thread f2))
+;(call-in-nested-thread f1 cust)
+;(call-in-nested-thread f2 cust)
 
 (define (f3)
   (define (f3-1)
@@ -24,12 +26,40 @@
                                                                    cust)))
                               cust)))
   (call-in-nested-thread f3-1 cust))
-(f3)
+;(f3)
+(define t3 (thread f3))
 
+(sync t1 t2 t3)
 
 '------------------------
 (define custbox (make-custodian-box (current-custodian) global))
 (custodian-box? custbox)
 (custodian-box-value custbox)
+
+'------------------------
+(letrec [[get-random-number (lambda () (random -999999 99999))]
+      [mlst (mlist 'start)]
+      [f1 (lambda ()
+            (do [[x 0 (get-random-number)]]
+              [[equal? x 1] [mappend! mlst (mlist x)]
+                            (displayln x)]))]
+      [f2 (lambda ()
+            (do [[x 0 (get-random-number)]]
+              [[equal? x 2] [mappend! mlst (mlist x)]
+                            (displayln x)]))]
+      [f3 (lambda ()
+            (do [[x 0 (get-random-number)]]
+              [[equal? x 3] [mappend! mlst (mlist x)]
+                            (displayln x)]))]
+      [f4 (lambda ()
+            (do [[x 0 (get-random-number)]]
+              [[equal? x 4] [mappend! mlst (mlist x)]
+                            (displayln x)]))]]
+  (let [[t1 (thread f1)]
+        [t2 (thread f2)]
+        [t3 (thread f3)]
+        [t4 (thread f4)]]
+    (begin (sync t1 t2 t3 t4)
+           mlst)))
 
 (custodian-shutdown-all cust)
