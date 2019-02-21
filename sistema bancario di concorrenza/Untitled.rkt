daec6963eb5ed760f64212e936dfda7c05d65f13
梁建溢 <a.124p@live.com>
2019-02-20 22:00:26 -0500

    Changesets-73

--- sistema bancario di concorrenza/Untitled.rkt
+++ sistema bancario di concorrenza/Untitled.rkt
@@ -1,36 +1,21 @@
 #lang racket
-;(require "./bank-view.rkt")
+(require "./bank-view.rkt")
+(require "../../JanyeeParallel/Serializer.rkt")
 
-(define (make-account balance)
-  (define (withdraw amount)
-    (if (>= balance amount)
-        (begin (set! balance (- balance amount))
-               balance)
-        "Insufficient funds"))
-  (define (deposit amount)
-    (set! balance (+ balance amount))
-    balance)
-  (define (dispatch m)
-    (cond ((eq? m 'withdraw) withdraw)
-          ((eq? m 'deposit) deposit)
-          ((eq? m 'balance) balance)
-          (else (error "Unknown request -- MAKE-ACCOUNT"
-                       m))))
-  dispatch)
-(define acc1 (make-account 100))
+(define acc1 (make-account-and-serializer-2 100))
 (define acc2 (make-account 39.4))
 (define acc3 (make-account 321.99))
 
 (define (while-away thunk x)
   (if [eq? x 111]
       (thunk)
-      (while-away thunk (random -999 999))))
+      (while-away thunk (random -9999 9999))))
 
 (define (withdraw-exe-80)
   (displayln ((acc1 'withdraw) 80)))
 
-(define (withdraw-exe-20)
-  (displayln ((acc1 'withdraw) 20)))
+(define (deposit-exe-20)
+  (displayln ((acc1 'deposit) 20)))
 
-(thread (while-away withdraw-exe-20 0))
-(thread (while-away withdraw-exe-80 0))
+(parallel-execute (lambda () (while-away deposit-exe-20 0))
+                  (lambda () (while-away withdraw-exe-80 0)))
