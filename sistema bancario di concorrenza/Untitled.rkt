57153a0abb15dc0be86a6c9f7564f6a182475ec6
梁建溢 <a.124p@live.com>
2019-02-22 06:50:28 -0500

    Update Untitled.rkt

--- sistema bancario di concorrenza/Untitled.rkt
+++ sistema bancario di concorrenza/Untitled.rkt
@@ -2,7 +2,7 @@
 (require "./bank-view.rkt")
 (require "../../JanyeeParallel/Serializer.rkt")
 
-(define acc1 (make-account-and-serializer-2 100))
+(define acc1 (make-account 100))
 ;(define acc2 (make-account 39.4))
 ;(define acc3 (make-account 321.99))
 
@@ -12,17 +12,35 @@
       (while-away thunk (random -9999 9999))))
 
 (define (withdraw-exe-80)
+  (displayln "Executing withdraw-exe-80")
   (displayln ((acc1 'withdraw) 80)))
 
 (define (deposit-exe-20)
+  (displayln "Executing deposit-exe-20")
   (displayln ((acc1 'deposit) 20)))
 
-(parallel-execute (lambda () (while-away deposit-exe-20 0))
-                  (lambda () (while-away withdraw-exe-80 0)))
 
+(define parallel1 (lambda ()
+                    (displayln "Executing parallel1")
+                    (parallel-execute (lambda () (while-away deposit-exe-20 0))
+                                      (lambda () (while-away withdraw-exe-80 0)))))
 
-(parallel-execute (lambda () (while-away deposit-exe-20 0))
-                  (lambda () (while-away withdraw-exe-80 0)))
+(define parallel2 (lambda ()
+                    (displayln "Executing parallel2")
+                    (parallel-execute (lambda () (while-away deposit-exe-20 0))
+                                      (lambda () (while-away withdraw-exe-80 0)))))
 
-(parallel-execute (lambda () (while-away deposit-exe-20 0))
-                  (lambda () (while-away withdraw-exe-80 0)))
+(define parallel3 (lambda ()
+                    (displayln "Executing parallel3")
+                    (parallel-execute (lambda () (while-away deposit-exe-20 0))
+                                      (lambda () (while-away withdraw-exe-80 0)))))
+
+(define serializer (make-serializer))
+(define serialize-parallel1 (serializer parallel1))
+(define serialize-parallel2 (serializer parallel2))
+(define serialize-parallel3 (serializer parallel3))
+
+(parallel-execute serialize-parallel1
+                  serialize-parallel2
+                  serialize-parallel3)
+
