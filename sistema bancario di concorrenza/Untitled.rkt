bbf472426252a315838e2eab3e16e17efccdcb82
梁建溢 <a.124p@live.com>
2019-02-28 04:16:36 -0500

    Changesets-76

--- sistema bancario di concorrenza/Untitled.rkt
+++ sistema bancario di concorrenza/Untitled.rkt
@@ -6,7 +6,7 @@
 ;(define acc2 (make-account 39.4))
 ;(define acc3 (make-account 321.99))
 
-(define (while-away thunk x)
+(define (while-away thunk [x 0])
   (if [eq? x 111]
       (thunk)
       (while-away thunk (random -9999 9999))))
@@ -22,25 +22,27 @@
 
 (define parallel1 (lambda ()
                     (displayln "Executing parallel1")
-                    (parallel-execute (lambda () (while-away deposit-exe-20 0))
-                                      (lambda () (while-away withdraw-exe-80 0)))))
+                    (parallel-execute (lambda () (while-away deposit-exe-20))
+                                      (lambda () (while-away withdraw-exe-80)))))
 
 (define parallel2 (lambda ()
                     (displayln "Executing parallel2")
-                    (parallel-execute (lambda () (while-away deposit-exe-20 0))
-                                      (lambda () (while-away withdraw-exe-80 0)))))
+                    (parallel-execute (lambda () (while-away deposit-exe-20))
+                                      (lambda () (while-away withdraw-exe-80)))))
 
 (define parallel3 (lambda ()
                     (displayln "Executing parallel3")
-                    (parallel-execute (lambda () (while-away deposit-exe-20 0))
-                                      (lambda () (while-away withdraw-exe-80 0)))))
+                    (parallel-execute (lambda () (while-away deposit-exe-20))
+                                      (lambda () (while-away withdraw-exe-80)))))
 
 (define serializer (make-serializer))
 (define serialize-parallel1 (serializer parallel1))
 (define serialize-parallel2 (serializer parallel2))
 (define serialize-parallel3 (serializer parallel3))
 
-(parallel-execute serialize-parallel1
+(define thd-lst (parallel-execute serialize-parallel1
                   serialize-parallel2
-                  serialize-parallel3)
+                  serialize-parallel3))
+(for ([thd thd-lst])
+  (thread-wait thd))
 
